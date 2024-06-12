1dabb7d3b9a87c7a0b4762b0556c354eee0f163a
Rusted2361 <hamzasajid4040@gmail.com>
2024-06-13 00:06:24 +0500

    implemented login signup backend

---
+++ backend/dist/utils/validators.js
@@ -0,0 +1,35 @@
+// import { Request, Response, NextFunction } from "express";
+// import { ValidationChain, body, validationResult } from "express-validator";
+import { body, validationResult } from "express-validator";
+const validate = (validations) => {
+    return async (req, res, next) => {
+        for (let validation of validations) {
+            await validation.run(req);
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                // If there are errors, send them back in the response
+                return res.status(422).json({ errors: errors.array() });
+            }
+        }
+        // If all validations pass, proceed to the next middleware
+        next();
+    };
+};
+const loginValidator = [
+    body("email").trim().isEmail().withMessage("Invalid email format"),
+    body("password")
+        .trim()
+        .isLength({ min: 6 })
+        .withMessage("password should at least have 6 characters"),
+];
+const signupValidator = [
+    body("name").not().isEmpty().withMessage("name is required"),
+    ...loginValidator,
+    //   body("email").trim().isEmail().withMessage("Invalid email format"), 
+    //   body("password")
+    //     .trim()
+    //     .isLength({ min: 6 })
+    //     .withMessage("password should at least have 6 characters"),
+];
+export { validate, signupValidator, loginValidator };
+//# sourceMappingURL=validators.js.map
--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,5 +1,7 @@
 import User from "../models/User.js";
 import { NextFunction, Request, Response } from "express";
+import { hash, compare } from "bcrypt";
+
 export const getAllUsers = async (
   req: Request,
   res: Response,
@@ -9,6 +11,57 @@ export const getAllUsers = async (
   try {
     const users = await User.find();
     return res.status(200).json({ message: "OK", users });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  //get all user
+  try {
+    //take values from request
+    const { name, email, password } = req.body;
+    //find user by email
+    const userexist = await User.findOne({ email });
+    if (userexist) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+    //hash password
+    const hashedPassword = await hash(password, 10);
+    //create new user
+    const user = new User({ name, email, password: hashedPassword });
+    //and store in database
+    await user.save();
+    return res.status(201).json({ message: "OK", id: user._id.toString() });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  //get all user
+  try {
+    //take values from request
+    const { email, password } = req.body;
+    //find user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email" });
+    }
+    //compare password
+    const isValid = await compare(password, user.password);
+    if (!isValid) {
+      return res.status(403).json({ message: "Invalid password" });
+    }
+    return res.status(200).json({ message: "OK", id: user.name.toString() });
   } catch (error) {
     return res.status(500).json({ message: "Internal Server Error" });
   }
